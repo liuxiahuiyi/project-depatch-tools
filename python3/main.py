46c6a7c18426af595602b2a2b6c19d7d6de84f1e
Aegon Wang <aegon.wang@farfetch.com>
2022-01-21 00:55:05 +0800

    UPDATE

--- python3/main.py
+++ python3/main.py
@@ -10,7 +10,6 @@ tables = [t for t in os.listdir(tables_dir) if re.match(r'table-\d+', t)]
 if len(tables) < 1:
   raise Exception(f'not found source table in ${tables_dir}')
 tables.sort(reverse = True)
-print(tables)
 table_latest = tables[0]
 table_new = f'table-{timestamp}.xlsx'
 depatch = Depatcher(os.path.join(tables_dir, table_latest), os.path.join(tables_dir, table_new))
